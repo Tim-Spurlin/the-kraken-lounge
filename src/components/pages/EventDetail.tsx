e1e92ed2040f8bf4f53a483150421f5a58f9a962
Tim Spurlin <178864942+Tim-Spurlin@users.noreply.github.com>
2026-03-12 10:44:07 +0000

    Generated by Spark: for the event Release the Kraken , here are two more audio overviews and hey are dropbox links so they need to be coonverteed. here is the english version https://www.dropbox.com/scl/fi/nhobtjkvkgrv8ktdy24za/Release_the_Kraken_Raves_and_Reefs.m4a?rlkey=nm6tnwm5503oc2hy3l3hbb27c&st=zpys66d7&dl=0 and here is the spanish version https://www.dropbox.com/scl/fi/ebu4tv1czqziz63f963c2/Ciencia_y_dise-o_de_Release_the_Kraken.m4a?rlkey=83h9jrvi6m38xqxp3eabq32nx&st=hqa9vmvs&dl=0

--- src/components/pages/EventDetail.tsx
+++ src/components/pages/EventDetail.tsx
@@ -11,6 +11,18 @@ import { Globe, SpeakerHigh } from '@phosphor-icons/react'
 const TRACKING_API_URL = 'https://script.google.com/macros/s/AKfycbxPaQivWxKDYw31OIbjOjphwOFG6C4Rp286fz1YmRnYEOJffvGVSdKSeo-tNPgcmJmu/exec'
 
 const EVENT_AUDIO_URLS: Record<string, { english: string; spanish: string }> = {
+  '1': {
+    english: 'https://dl.dropboxusercontent.com/scl/fi/nhobtjkvkgrv8ktdy24za/Release_the_Kraken_Raves_and_Reefs.m4a?rlkey=nm6tnwm5503oc2hy3l3hbb27c&st=zpys66d7',
+    spanish: 'https://dl.dropboxusercontent.com/scl/fi/ebu4tv1czqziz63f963c2/Ciencia_y_dise-o_de_Release_the_Kraken.m4a?rlkey=83h9jrvi6m38xqxp3eabq32nx&st=hqa9vmvs'
+  },
+  '2': {
+    english: 'https://res.cloudinary.com/dw3lf8roj/video/upload/v1773288866/Why_Heavy_Noise_Cures_Modern_Anxiety_pwleyp.mp4',
+    spanish: 'https://res.cloudinary.com/dw3lf8roj/video/upload/v1773288899/M%C3%BAsica_oscura_como_herramienta_de_supervivencia_rsgftd.mp4'
+  },
+  '3': {
+    english: 'https://dl.dropboxusercontent.com/scl/fi/uf03itkdfagqfns0g213c/Dark_Alternative_Collision_in_Brownsville.m4a?rlkey=d5lo2j62obd2uw4ee10cfvgrc&st=0qd52thg',
+    spanish: 'https://res.cloudinary.com/dw3lf8roj/video/upload/v1773288899/M%C3%BAsica_oscura_como_herramienta_de_supervivencia_rsgftd.mp4'
+  },
   '4': {
     english: 'https://www.dropbox.com/scl/fi/eq6782sfui2kzxkgkx68p/German_industrial_legends_Das_Ich_in_Brownsville.m4a?rlkey=d3h7uusk4xrb0s8svwtpvhy0q&st=eiyf5gzo&dl=1',
     spanish: 'https://www.dropbox.com/scl/fi/2m0layi9i35jm6v12bdx1/Das_Ich_conquista_Brownsville.m4a?rlkey=ltdfvqvbmzasmsgz0ffzbvzka&st=qs3xw2n2&dl=1'
@@ -23,17 +35,9 @@ const EVENT_AUDIO_URLS: Record<string, { english: string; spanish: string }> = {
     english: 'https://www.dropbox.com/scl/fi/vrxbmgiazx2hf19iji86f/Brownsville_s_First_Friday_Goth_Night_Sanctuary.m4a?rlkey=yve8smahs8oqryym4p4qpvwqn&st=04sk6q2f&dl=1',
     spanish: 'https://www.dropbox.com/scl/fi/kz8064avmqaqtcxeeul3m/G-ticos_impulsando_el_centro_de_Brownsville.m4a?rlkey=2vgqop1s503pnadvneqqaor9o&st=7dretta6&dl=1'
   },
-  '2': {
-    english: 'https://res.cloudinary.com/dw3lf8roj/video/upload/v1773288866/Why_Heavy_Noise_Cures_Modern_Anxiety_pwleyp.mp4',
-    spanish: 'https://res.cloudinary.com/dw3lf8roj/video/upload/v1773288899/M%C3%BAsica_oscura_como_herramienta_de_supervivencia_rsgftd.mp4'
-  },
   '8': {
     english: 'https://dl.dropboxusercontent.com/scl/fi/8qa406tsq8hwhj5thmqog/Underground_Techno_Cures_Sunday_Scaries.m4a?rlkey=0ns4ltliqd0gel0n7mxu86imp&st=c9ltwtp5',
     spanish: 'https://dl.dropboxusercontent.com/scl/fi/3jtxdy6k6hixoomqax7ir/Terapia_de_techno_dominical_en_Brownsville.m4a?rlkey=s9sxxi8cgh7bu25631z4l1f6o&st=0zxner97'
-  },
-  '3': {
-    english: 'https://dl.dropboxusercontent.com/scl/fi/uf03itkdfagqfns0g213c/Dark_Alternative_Collision_in_Brownsville.m4a?rlkey=d5lo2j62obd2uw4ee10cfvgrc&st=0qd52thg',
-    spanish: 'https://res.cloudinary.com/dw3lf8roj/video/upload/v1773288899/M%C3%BAsica_oscura_como_herramienta_de_supervivencia_rsgftd.mp4'
   }
 }
 
